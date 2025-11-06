184eef50c37271e45b7ad56cca16be7f56efebcd
Emre <147698967+rixemre@users.noreply.github.com>
2025-11-06 12:06:13 +0300

    Update weather_models.dart

--- models/weather_models.dart
+++ models/weather_models.dart
@@ -2,12 +2,14 @@ class WeatherModels {
   final String gun;
   final double max;
   final double min;
+  final double weatherCode;
   final String birim;
 
   WeatherModels({
     required this.gun,
     required this.max,
     required this.min,
+    required this.weatherCode,
     required this.birim,
   });
 }
